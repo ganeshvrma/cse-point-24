4c0614a1e861c043b71d7376334718effab601c7
ganeshvrma <135155713+ganeshvrma@users.noreply.github.com>
2025-02-16 14:29:56 +0530

    del/files

--- script.js
+++
@@ -1,9 +0,0 @@
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navbarLinks = document.querySelector(".navbar-links");
-
-    menuIcon.addEventListener("click", function() {
-      navbarLinks.classList.toggle("show");
-    });
-  });
